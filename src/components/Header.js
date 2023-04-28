f99f2d2df929b98a96a4b996c52fe6816767f2cc
Dennis Bastkowski <d.bastkowski@googlemail.com>
2023-04-28 16:34:47 +0200

    Header development (WIP)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,51 @@
 import React from 'react'
-
+import { BsSearch } from 'react-icons/bs'
+import { Link, NavLink } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+        <header className="header-top-strip py-3">
+            <div className="container-xxl">
+                <div className="row">
+                    <div className="col-6">
+                        <p className='text-white mb-0'>
+                        Something about shipping and returns.
+                        </p>
+                    </div>
+                    <div className="col-6">
+                        <p className='text-end text-white mb-0'>
+                            Hotline:
+                            <a className='text-white' href="tel:+1232343215135">+12 32343215135</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <header className="header-upper py-3">
+            <div className="container-xxl">
+                <div className="row align-items-center">
+                    <div className="col-2">
+                        <h2><Link className='text-white'>Dev Corner</Link></h2>
+                    </div>
+                    <div className="col-5">
+                        <div className="input-group">
+                            <input 
+                                type="text" 
+                                className="form-control py-2" 
+                                placeholder="Search product here..." 
+                                aria-label="Search product here..." 
+                                aria-describedby="basic-addon2"
+                            />
+                            <span className="input-group-text p-3" id="basic-addon2">
+                                <BsSearch className='fs-5'/>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="col-5"></div>
+                </div>
+            </div>
+        </header>
+    </>
   )
 }
 
